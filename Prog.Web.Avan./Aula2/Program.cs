ea49406dfee945d5b6ffc9c6a23c50066847969b
Jessica <jessica.laine@engenharia.ufjf.br>
2024-02-20 21:50:31 -0300

    .

---
+++ Prog.Web.Avan./Aula2/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Data.Sqlite;
+
+using (var conexao = new SqliteConnection(@"Data Source=db/dados.db"))
+{
+    conexao.Open();
+
+    //conexao.Close(); vai ser automaticco se usar o using
+}
+
