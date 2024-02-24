5cdb9cf6a8f483a8ad38a4493e18a211828f1fb5
Jessica <jessica.laine@engenharia.ufjf.br>
2024-02-23 21:57:54 -0300

    .

--- Prog.Web.Avan./Aula2/Program.cs
+++ Prog.Web.Avan./Aula2/Program.cs
@@ -4,6 +4,34 @@ using (var conexao = new SqliteConnection(@"Data Source=db/dados.db"))
 {
     conexao.Open();
 
-    //conexao.Close(); vai ser automaticco se usar o using
+    var cmd = conexao.CreateCommand();
+    /*
+    cmd.CommandText="SELECT * FROM contato";
+    var dr = cmd.ExecuteReader();
+
+    while (dr.Read())
+    {
+        Console.WriteLine($"{dr["nome"]}: {dr["gmail"]}");
+    }
+    dr.Close();*/
+
+    /*var cmd = conexao.CreateCommand();*/
+
+    /*cmd.CommandText = "INSERT INTO contato(id, nome, gmail)" + 
+                      "VALUES (@id, @nome, @gmail)";
+    cmd.Parameters.AddWithValue("@id", 4);
+    cmd.Parameters.AddWithValue("@nome", "Daniel");
+    cmd.Parameters.AddWithValue("@gmail", "daniel@gmail.com");*/
+
+    cmd.CommandText = "UPDATE contato SET nome=@nome, gmail=@gmail WHERE id=@id";
+    cmd.Parameters.AddWithValue("@nome", "Daniel da Silva");
+    cmd.Parameters.AddWithValue("@id", 4);
+    cmd.Parameters.AddWithValue("@gmail", "daniel@gmail.com");
+
+    /*cmd.CommandText = "DELETE from contato where id=@id";
+    cmd.Parameters.AddWithValue("@id", "3");*/
+
+    cmd.ExecuteNonQuery();
+    conexao.Close(); //vai ser automatico se usar o using
 }
 
